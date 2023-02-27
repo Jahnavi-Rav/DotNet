ceee10a70817624f453647d5f93efd41c6cde410
Jahnavi-Rav <65764268+Jahnavi-Rav@users.noreply.github.com>
2023-02-26 22:15:00 -0600

    Add files via upload

---
+++ CoreWebAPI/Models/TblEducationalDetailsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DOTNET_CORE_WEB_API_DEMO.Models;
+
+namespace DOTNET_CORE_WEB_API_DEMO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TblEducationalDetailsController : ControllerBase
+    {
+        private readonly DbEmployeeContext _context;
+
+        public TblEducationalDetailsController(DbEmployeeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TblEducationalDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblEducationalDetail>>> GetTblEducationalDetails()
+        {
+            return await _context.TblEducationalDetails.ToListAsync();
+        }
+
+        // GET: api/TblEducationalDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TblEducationalDetail>> GetTblEducationalDetail(int id)
+        {
+            var tblEducationalDetail = await _context.TblEducationalDetails.FindAsync(id);
+
+            if (tblEducationalDetail == null)
+            {
+                return NotFound();
+            }
+
+            return tblEducationalDetail;
+        }
+
+        // PUT: api/TblEducationalDetails/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTblEducationalDetail(int id, TblEducationalDetail tblEducationalDetail)
+        {
+            if (id != tblEducationalDetail.EducationalDetailsId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tblEducationalDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblEducationalDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TblEducationalDetails
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TblEducationalDetail>> PostTblEducationalDetail(TblEducationalDetail tblEducationalDetail)
+        {
+            _context.TblEducationalDetails.Add(tblEducationalDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTblEducationalDetail", new { id = tblEducationalDetail.EducationalDetailsId }, tblEducationalDetail);
+        }
+
+        // DELETE: api/TblEducationalDetails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTblEducationalDetail(int id)
+        {
+            var tblEducationalDetail = await _context.TblEducationalDetails.FindAsync(id);
+            if (tblEducationalDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.TblEducationalDetails.Remove(tblEducationalDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TblEducationalDetailExists(int id)
+        {
+            return _context.TblEducationalDetails.Any(e => e.EducationalDetailsId == id);
+        }
+    }
+}
---
+++ CoreWebAPI/Models/TblUserMastersController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DOTNET_CORE_WEB_API_DEMO.Models;
+
+namespace DOTNET_CORE_WEB_API_DEMO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TblUserMastersController : ControllerBase
+    {
+        private readonly DbEmployeeContext _context;
+
+        public TblUserMastersController(DbEmployeeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TblUserMasters
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblUserMaster>>> GetTblUserMasters()
+        {
+            return await _context.TblUserMasters.ToListAsync();
+        }
+
+        // GET: api/TblUserMasters/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TblUserMaster>> GetTblUserMaster(int id)
+        {
+            var tblUserMaster = await _context.TblUserMasters.FindAsync(id);
+
+            if (tblUserMaster == null)
+            {
+                return NotFound();
+            }
+
+            return tblUserMaster;
+        }
+
+        // PUT: api/TblUserMasters/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTblUserMaster(int id, TblUserMaster tblUserMaster)
+        {
+            if (id != tblUserMaster.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tblUserMaster).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblUserMasterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TblUserMasters
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TblUserMaster>> PostTblUserMaster(TblUserMaster tblUserMaster)
+        {
+            _context.TblUserMasters.Add(tblUserMaster);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTblUserMaster", new { id = tblUserMaster.UserId }, tblUserMaster);
+        }
+
+        // DELETE: api/TblUserMasters/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTblUserMaster(int id)
+        {
+            var tblUserMaster = await _context.TblUserMasters.FindAsync(id);
+            if (tblUserMaster == null)
+            {
+                return NotFound();
+            }
+
+            _context.TblUserMasters.Remove(tblUserMaster);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TblUserMasterExists(int id)
+        {
+            return _context.TblUserMasters.Any(e => e.UserId == id);
+        }
+    }
+}

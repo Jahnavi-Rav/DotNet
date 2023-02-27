b151dc50a34ac60f3103d8f6f64719370088b244
Jahnavi-Rav <65764268+Jahnavi-Rav@users.noreply.github.com>
2023-02-26 21:52:52 -0600

    Add files via upload

---
+++ CoreWebAPI/Program.cs
@@ -0,0 +1,30 @@
+using DOTNET_CORE_WEB_API_DEMO.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext <DbEmployeeContext> (options => options.UseSqlServer(builder.Configuration.GetConnectionString("CORE_WEB_APIContext") 
+    ?? throw new InvalidOperationException("Connection string 'CORE_WEB_APIContext' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
